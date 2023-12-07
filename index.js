99f4fce9d005a2f4c8ab8e36790535797d8179c0
louisoduor <louisodhiambo20@gmail.com>
2023-12-07 17:03:40 +0000

    Completed assignmet

---
+++ index.js
@@ -0,0 +1,39 @@
+ function shout(string) {
+    return string.toUpperCase();
+  }
+ function whisper(string){
+    return string.toLowerCase();
+}
+
+
+ function whisper(string){
+    console.log( string.toUpperCase());
+
+}
+
+ function logShout(string){
+    console.log( string.toLowerCase());
+}
+
+ function logShout(string){
+    console.log(string.toUpperCase());
+ }
+
+  function logWhisper (string){
+    console.log(string.toLowerCase());
+ }
+
+  function logWhisper(string){
+    console.log(string.toUpperCase());
+ }
+
+  function sayHiToGrandma (){
+    console.log(string.toLowerCase());
+   
+    
+ }
+  
+ 
+
+  
+
